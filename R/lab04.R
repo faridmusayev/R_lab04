f160957ebe883a2cf211f69b1076b8c64c6640ba
faridmusayev <mfarid994@gmail.com>
2021-09-21 20:08:28 +0200

    update package documentation

---
+++ R/lab04.R
@@ -0,0 +1,21 @@
+#' Package with Multiple Linear Regression
+#'
+#'Reference Class included:
+#' \code{linreg}
+#'
+#'@details
+#'
+#'For further information see \code{linreg} class documentation.
+#'
+#'@author
+#'
+#'- Farid Musayev   (gitHub:"faridmusayev", liuid:farmu437)
+#'- Mucahit Sahin   (gitHub:"mchtsahin", liuid:mucsa806)
+#'- Selen Karaduman (gitHub:"selenkaraduman", liuid:selka266)
+#'
+#'
+#'
+#' @docType package
+#' @name lab04
+NULL
+#> NULL
